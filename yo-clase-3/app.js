8e2d0fa0391538b91ffbf5351bade190d1a29d9b
alsaenz <157411527+agusaenz@users.noreply.github.com>
2024-08-26 11:40:43 -0300

    ult upd clase3

--- yo-clase-3/app.js
+++ yo-clase-3/app.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const movies = require('./movies.json')
 const crypto = require('node:crypto')
+const cors = require('cors')
 const z = require('zod')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
-app.disable('x-powered-by')
 app.use(express.json())
+app.use(cors({
+    origin: (origin, callback) => {
+        const ACCEPTED_ORIGINS = [
+            'http://localhost:8080',
+            'http://localhost:3000',
+            'http://localhost:1234'
+        ]
+
+        if (ACCEPTED_ORIGINS.includes(origin)) {
+            return callback(null, true)
+        }
+
+        if (!origin) {
+            return callback(null, true)
+        }
+
+        return callback(new Error('Not allowed by CORS'))
+    }
+}))
+app.disable('x-powered-by')
 
 // for CORS
 // normal methods: GET, HEAD, POST
@@ -15,18 +35,9 @@ app.use(express.json())
 // CORS PRE-FLIGHT
 // OPTIONS 
 
-const ACCEPTED_ORIGINS = [
-    'http://localhost:8080',
-    'http://localhost:3000',
-    'http://localhost:1234'
-]
+
 
 app.get('/movies', (req, res) => {
-    const origin = req.header('origin')
-    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-        res.header('Access-Control-Allow-Origin', origin)
-    }
-
     const { genre } = req.query
     if (genre) {
         const filteredMovies = movies.filter(
@@ -88,11 +99,6 @@ app.patch('/movies/:id', (req, res) => {
 })
 
 app.delete('/movies/:id', (req, res) => {
-    const origin = req.header('origin')
-    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-        res.header('Access-Control-Allow-Origin', origin)
-    }
-
     const { id } = req.params
     const movieIndex = movies.findIndex(movie => movie.id === id)
 
@@ -103,17 +109,6 @@ app.delete('/movies/:id', (req, res) => {
     movies.splice(movieIndex, 1)
 
     res.json({ message: 'Movie deleted' })
-})
-
-app.options('/movies/:id', (req, res) => {
-    const origin = req.header('origin')
-
-    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-        res.header('Access-Control-Allow-Origin', origin)
-        res.header('Access-Control-Allow-Methods', 'DELETE, PATCH, PUT, OPTIONS, GET')
-    }
-
-    res.send(200)
 })
 
 const PORT = process.env.PORT ?? 1234
